46f2685b59a816184962b66dd974c5bb364ffa79
aakash <aakashgarg100@gmail.com>
2019-12-01 18:58:32 +0530

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,9 @@
-
-
-
-fetch('/weather?address=').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error)
-        }
-        else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-// messageOne.textContent = 'fdsfe'
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
